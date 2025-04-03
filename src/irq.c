8c42e69b5aaa483ec4348162004945f88c763905
Rui Zhang <rui.zhang@aigcic.com>
2025-04-03 18:04:31 +0800

    update irq.c

--- src/irq.c
+++ src/irq.c
@@ -2,6 +2,7 @@
 
 static Context* do_event(Event e, Context* c) {
   switch (e.event) {
+    case EVENT_YIELD: Log("EVENT_YIELD: cause = %d", c->mcause); c->mepc += 4; break;
     default: panic("Unhandled event ID = %d", e.event);
   }
 
