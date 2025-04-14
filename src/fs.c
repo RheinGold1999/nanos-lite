ad9fa5b7ffdac3f0c3cf9b620384783314f01b1b
Rui Zhang <rui.zhang@aigcic.com>
2025-04-14 15:39:17 +0800

    add sfs functions

--- src/fs.c
+++ src/fs.c
@@ -7,6 +7,7 @@ typedef struct {
   char *name;
   size_t size;
   size_t disk_offset;
+  size_t open_offset;
   ReadFn read;
   WriteFn write;
 } Finfo;
@@ -25,12 +26,74 @@ size_t invalid_write(const void *buf, size_t offset, size_t len) {
 
 /* This is the information about all files in disk. */
 static Finfo file_table[] __attribute__((used)) = {
-  [FD_STDIN]  = {"stdin", 0, 0, invalid_read, invalid_write},
-  [FD_STDOUT] = {"stdout", 0, 0, invalid_read, invalid_write},
-  [FD_STDERR] = {"stderr", 0, 0, invalid_read, invalid_write},
+  [FD_STDIN]  = {"stdin", 0, 0, 0, invalid_read, invalid_write},
+  [FD_STDOUT] = {"stdout", 0, 0, 0, invalid_read, invalid_write},
+  [FD_STDERR] = {"stderr", 0, 0, 0, invalid_read, invalid_write},
 #include "files.h"
 };
 
 void init_fs() {
   // TODO: initialize the size of /dev/fb
+}
+
+extern size_t ramdisk_read(void *buf, size_t offset, size_t len);
+extern size_t ramdisk_write(const void *buf, size_t offset, size_t len);
+
+const char *fs_name(int fd) {
+  assert(0 <= fd && fd < LENGTH(file_table));
+  return file_table[fd].name;
+}
+
+int fs_open(const char *pathname, int flags, int mode) {
+  for (int i = 0; i < LENGTH(file_table); ++i) {
+    if (strcmp(pathname, file_table[i].name) == 0) {
+      file_table[i].open_offset = 0;
+      return i;
+    }
+  }
+  panic("pathname is invalid: %s\n", pathname);
+}
+
+size_t fs_read(int fd, void *buf, size_t len) {
+  if (fd == FD_STDOUT && fd == FD_STDERR) {
+    len = 0;
+  } else if (fd != FD_STDIN) {
+    Finfo *finfo = &file_table[fd];
+    len = (finfo->size - finfo->open_offset) < len ? (finfo->size - finfo->open_offset) : len;
+    ramdisk_read(buf, (finfo->disk_offset + finfo->open_offset), len);
+    finfo->open_offset += len;
+  }
+  return len;
+}
+
+size_t fs_write(int fd, const void *buf, size_t len) {
+  if (fd == FD_STDOUT && fd == FD_STDERR) {
+    for (size_t i = 0; i < len; ++i) {
+      putch(((const unsigned char *)buf)[i]);
+    }
+  } else if (fd != FD_STDIN) {
+    Finfo *finfo = &file_table[fd];
+    len = (finfo->size - finfo->open_offset) < len ? (finfo->size - finfo->open_offset) : len;
+    ramdisk_write(buf, (finfo->disk_offset + finfo->open_offset), len);
+    finfo->open_offset += len;
+  }
+  return len;
+}
+
+size_t fs_lseek(int fd, size_t offset, int whence) {
+  Finfo *finfo = &file_table[fd];
+  if (fd != FD_STDIN && fd != FD_STDOUT && fd != FD_STDERR) {
+    if (whence == SEEK_SET) {
+      finfo->open_offset = offset;
+    } else if (whence == SEEK_CUR) {
+      finfo->open_offset += offset;
+    } else if (whence == SEEK_END) {
+      finfo->open_offset = finfo->size + offset;
+    }
+  }
+  return finfo->open_offset;
+}
+
+int fs_close(int fd) {
+  return 0;
 }
--- src/loader.c
+++ src/loader.c
@@ -1,5 +1,6 @@
 #include <proc.h>
 #include <elf.h>
+#include <fs.h>
 
 #ifdef __LP64__
 # define Elf_Ehdr Elf64_Ehdr
@@ -9,16 +10,55 @@
 # define Elf_Phdr Elf32_Phdr
 #endif
 
+extern int fs_open(const char *pathname, int flags, int mode);
+extern size_t fs_read(int fd, void *buf, size_t len);
+extern int fs_close(int fd);
+
 static uintptr_t loader(PCB *pcb, const char *filename) {
   // TODO();
 
-  extern size_t ramdisk_read(void *buf, size_t offset, size_t len);
+  // extern size_t ramdisk_read(void *buf, size_t offset, size_t len);
 
-  ramdisk_read((void *)0x83000000, 0, 0x06e60);
-  ramdisk_read((void *)0x83007000, 0x007000, 0x008a0);
+  // Elf_Ehdr ehdr;
+  // ramdisk_read(&ehdr, 0, sizeof(ehdr));
+  // // check elf magic number
+  // assert(*((uint32_t *)ehdr.e_ident) == 0x464c457f);
 
-  memset((void *)(0x83007000 + 0x008a0), 0, (0x008e0 - 0x008a0));
-  return (uintptr_t) 0x83004ee8;
+  // Elf_Phdr phdr[ehdr.e_phnum];
+  // ramdisk_read(&phdr, ehdr.e_phoff, sizeof(phdr)*ehdr.e_phnum);
+  // for (size_t i = 0; i < ehdr.e_phnum; ++i) {
+  //   if (phdr[i].p_type == PT_LOAD) {
+  //     ramdisk_read((void *)phdr[i].p_vaddr, phdr[i].p_offset, phdr[i].p_filesz);
+  //     memset((void*)(phdr[i].p_vaddr + phdr[i].p_filesz), 0, (phdr[i].p_memsz - phdr[i].p_filesz));
+  //   }
+  // }
+
+  // return (uintptr_t) ehdr.e_entry;
+
+  int fd = fs_open(filename, 0, 0);
+  Elf_Ehdr ehdr;
+  fs_read(fd, &ehdr, sizeof(ehdr));
+  // const Elf_Ehdr ehdr = ehdr_tmp;
+  // check elf magic number
+  assert(*((uint32_t *)ehdr.e_ident) == 0x464c457f);
+  Log("fd: %d, filename: %s", fd, fs_name(fd));
+  Log("entry: %p", ehdr.e_entry);
+
+  Elf_Phdr phdr[ehdr.e_phnum];
+  fs_lseek(fd, ehdr.e_phoff, SEEK_SET);
+  fs_read(fd, &phdr, sizeof(phdr));
+  for (size_t i = 0; i < ehdr.e_phnum; ++i) {
+    if (phdr[i].p_type == PT_LOAD) {
+      // ramdisk_read((void *)phdr[i].p_vaddr, phdr[i].p_offset, phdr[i].p_filesz);
+      Log("LOAD: Offset: %p, VirtAddr: %p, FileSiz: %p, MemSiz: %p", 
+        phdr[i].p_offset, phdr[i].p_vaddr, phdr[i].p_filesz, phdr[i].p_memsz);
+      fs_lseek(fd, phdr[i].p_offset, SEEK_SET);
+      fs_read(fd, (void *)phdr[i].p_vaddr, phdr[i].p_filesz);
+      memset((void*)(phdr[i].p_vaddr + phdr[i].p_filesz), 0, (phdr[i].p_memsz - phdr[i].p_filesz));
+    }
+  }
+
+  return (uintptr_t)ehdr.e_entry;
 }
 
 void naive_uload(PCB *pcb, const char *filename) {
